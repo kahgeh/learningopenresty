4e9ba7655280f9fb360ea3b453c27e98b695f398
dev-kahgehtan <kahgeh.tan@xero.com>
2018-05-19 10:37:23 +1000

    - sample to show look up

--- jsonmanipulation.lua
+++
@@ -1,6 +0,0 @@
--- learn how to manipulate object, filter
-local cjson = require "cjson"
-
-local my_json = [[{"my_array":[]}]]
-require 'pl.pretty'.dump( cjson.decode(my_json) )
-
---
+++ lookuptable.lua
@@ -0,0 +1,28 @@
+-- learn how to manipulate object, filter
+local cjson = require "cjson"
+local pl = require 'pl.pretty'
+local my_json = [[{"my_array":[]}]]
+
+local file = io.open("mockdns/app/public/names.json")
+
+function filter(enumerable, predicate)
+    local result={}
+    for index,value in ipairs(enumerable) do
+        if(predicate(value,index)) then
+            result[#result+1]=value
+        end
+    end
+    return result
+end
+
+function lookup(name, names )
+    local result=filter(names, function(value,index) return value['name'] == name end )
+    
+    if #result > 0  then
+        return result[1].address
+    end      
+    return nil
+end
+
+print (lookup('echo-fiji', cjson.decode(file:read("*a") )) )
+
